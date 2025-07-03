b39a4c5426466ac64890a32a9a313ffeee8bf210
Jay-Patel-Simform <jay.a.patel@simformsolutions.com>
2025-07-03 11:16:41 +0530

    Add initial project setup with Tailwind CSS, Radix UI, and custom Button component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,9 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+import { Button } from "~/components/ui/button"
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
 }
